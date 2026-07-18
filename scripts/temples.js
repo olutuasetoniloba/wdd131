ebf8a7d9d017e8e43dfb55d17854a4f36c15d1b7
olutuasetoniloba <tolutuase@byupathway.edu>
2026-07-19 00:32:38 +0100

    assingment

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+temples.js
+
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
